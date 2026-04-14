46fc61d3815dd603aa84f387ccdb97f327e0ebff
gcarb025 <gabycarbone@outlook.com>
2026-04-13 23:45:01 -0400

    finishing temporary tables

--- Temporary_Tables.sql
+++ Temporary_Tables.sql
@@ -92,3 +92,44 @@ select * from dates d1 union all select * from cte c;
 drop table if exists f_highest_salaries;
 drop temporary table if exists dates;
 
+#Create a temporary table called dates containing the following three columns:
+#- one displaying the current date and time,
+#- another one displaying two months earlier than the current date and time, and a
+#- third column displaying two years later than the current date and time.
+
+create temporary table dates 
+select 
+	now() as current_date_and_time,
+	date_sub(now(),interval 2 month) as two_months_earlier,
+	date_sub(now(), interval -2 year) as two_years_later;
+
+#Write a query\ that allows you to check the result set contained in the dates temporary table you created in the previous exercise.
+select * from dates;
+
+#Create a query joining the result sets from the dates temporary table you created during the previous lecture with a new 
+#Common Table Expression (CTE) containing the same columns. Let all columns in the result set appear on the same row.
+
+with cte as (
+select 
+	now() as current_date_and_time,
+	date_sub(now(),interval 2 month) as two_months_earlier,
+	date_sub(now(), interval -2 year) as two_years_later)
+select * from dates d1 join cte c;
+
+#Again, create a query joining the result sets from the dates temporary table you created during the previous lecture with a new 
+#Common Table Expression (CTE) containing the same columns. This time, combine the two sets vertically.
+
+with cte as (
+select 
+	now() as current_date_and_time,
+	date_sub(now(),interval 2 month) as two_months_earlier,
+	date_sub(now(), interval -2 year) as two_years_later)
+select * from dates d1 union all select * from cte c;
+
+#Drop the male_max_salaries and dates temporary tables you recently created.
+drop table if exists male_max_salaries;
+drop table if exists dates;
+
+
+
+
